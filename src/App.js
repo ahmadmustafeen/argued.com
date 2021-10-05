5fc5089874d993d3d63f5795393d6616fae87766
ahmadmustafeen <engr.ahmadmustafeen@gmail.com>
2021-10-06 01:45:17 +0500

    otp screen completed

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import './App.css';
 import Signin from './containers/Signin';
 import Signup from './containers/Signup';
 import RequestReset from './containers/RequestReset'
+import OTPScreen from './containers/OTPScreen'
 
 function App() {
   return (
     // <Signin />
     // <Signup />
-    <RequestReset />
+    // <RequestReset />
+    <OTPScreen />
   );
 }
 
