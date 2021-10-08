074815c9f0fbd06cf87d7a84cdf1c93a27e16f5c
ahmadmustafeen <engr.ahmadmustafeen@gmail.com>
2021-10-08 09:03:59 +0500

    AppModal completed

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+import React,{useState,useEffect} from 'react'
 import Signin from './containers/Signin';
 import Signup from './containers/Signup';
 import RequestReset from './containers/RequestReset'
@@ -7,10 +8,23 @@ import OTPScreen from './containers/OTPScreen'
 import ResetPassword from './containers/ResetPassword'
 import {Switch, Route} from 'react-router-dom'
 import Dashboard from './containers/Dashboard';
+import {AppModal}  from './components'
 
 function App() {
+  const [isVisible,setIsVisible] = useState(false)
+  useState(()=>{
+    setTimeout(()=>{
+      setIsVisible(true)
+    },2000)
+    setTimeout(()=>{
+      setIsVisible(false)
+    },6000)
+  },[])
   return (
+    <>
+    {isVisible && <AppModal />}
     <Dashboard />
+    </>
     // <Switch>
     //   <Route path="/signin" exact component={Signin} />
     //   <Route path="/signup" exact  component={Signup} />
---
+++ src/components/AppModal/index.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import './style.css'
+import {WelcomeModal} from '../'
+
+
+const AppModal = props => {
+    return (
+        <div className="AppModal">
+<WelcomeModal />
+        </div>
+    )
+} 
+
+
+export  {AppModal}
