85095c4aefd6aa557aab62512e7a71fcb510d963
ahmadmustafeen <engr.ahmadmustafeen@gmail.com>
2021-10-11 19:59:08 +0500

    popover modal completed

--- src/components/DashboardHeader/index.js
+++ src/components/DashboardHeader/index.js
@@ -3,10 +3,41 @@ import './style.css'
 import { theme } from '../../constants/theme'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faChevronCircleRight, faBars, faTimesCircl, faSearch, faChevronDown, } from '@fortawesome/free-solid-svg-icons'
-
+import { Button, Popover, Typography } from '@material-ui/core'
 
 
 const DashboardHeader = props => {
+    const [anchorEl, setAnchorEl] = React.useState(null);
+
+    const handleClick = (event) => {
+        setAnchorEl(event.currentTarget);
+    };
+
+    const handleClose = () => {
+        setAnchorEl(null);
+    };
+
+
+    // this here is the code of popover
+    {/* <Button aria-describedby={id} variant="contained" onClick={handleClick}>
+  Open Popover
+</Button>
+<Popover
+  id={id}
+  open={open}
+  anchorEl={anchorEl}
+  onClose={handleClose}
+  anchorOrigin={{
+    vertical: 'bottom',
+    horizontal: 'left',
+  }}
+>
+<Typography sx={{ p: 2 }}>The content of the Popover.</Typography>
+</Popover>
+  */}
+    const open = Boolean(anchorEl);
+    const id = open ? 'simple-popover' : undefined;
+
     return (
         <div className="dashboardheader-container">
             <div className="dashboardheader-container-left">
@@ -30,17 +61,17 @@ const DashboardHeader = props => {
                 <div className="dashboardheader-container-middle-searchbar">
                     <div className="dashboardheader-container-middle-searchbar-container">
                         <div className="dashboardheader-container-middle-searchbar-container-left">
-                       <h4 style={{color:theme.fontColor.whiteText}}>
-                           Videos
-                       </h4>
-                        <FontAwesomeIcon icon={faChevronDown}
+                            <h4 style={{ color: theme.fontColor.whiteText }}>
+                                Videos
+                            </h4>
+                            <FontAwesomeIcon icon={faChevronDown}
                                 color={theme.fontColor.darkBlue}
                             />
                         </div>
                         <div className="dashboardheader-container-middle-searchbar-container-middle">
-                            <input 
-                            style={{color:theme.fontColor.whiteText}}
-                            placeholder="Type anything here" />
+                            <input
+                                style={{ color: theme.fontColor.whiteText }}
+                                placeholder="Type anything here" />
                         </div>
                         <div className="dashboardheader-container-middle-searchbar-container-right">
                             <FontAwesomeIcon icon={faSearch}
